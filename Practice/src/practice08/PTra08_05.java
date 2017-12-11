12ad840d5bcf286a1979244d65e45031e674b6b7
DESKTOP-S120DCG\likeit_student <harasho2117@gmail.com>
2017-12-11 18:54:10 +0900

    9-1まで終了。

--- Practice/src/practice08/PTra08_05.java
+++ Practice/src/practice08/PTra08_05.java
@@ -32,7 +32,15 @@ public class PTra08_05 {
 		int choice = scanner.nextInt();
 
 		// ★ 変数choiceの中が、1であれば四角形の面積を算出するメソッドを、2であれば三角形の面積を算出するメソッドを呼び出してください
+		switch(choice){
+		case 1:
+			System.out.println(quadrangle(width,height));
+			break;
 
+		case 2:
+			System.out.println(triangle(width,height));
+			break;
+		}
 	}
 
 	/*
@@ -44,6 +52,10 @@ public class PTra08_05 {
 	 *
 	 * 引数で渡された横の長さ(width)と縦の長さ(height)から、四角形の面積を返すメソッド
 	 */
+	public static double quadrangle(double width, double heigh) {
+		double answer= (heigh* width);
+		return answer;
+	}
 
 
 	/*
@@ -55,5 +67,8 @@ public class PTra08_05 {
 	 *
 	 * 引数で渡された横の長さ(width)と縦の長さ(height)から、三角形の面積を返すメソッド
 	 */
-
+	public static double triangle(double width, double heigh) {
+		double answer= (heigh* width/ 2);
+		return answer;
+	}
 }
