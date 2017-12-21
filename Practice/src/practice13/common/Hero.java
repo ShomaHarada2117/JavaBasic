d0b2e2907859714e9f827985ceb265ff1a914a50
DESKTOP-S120DCG\likeit_student <harasho2117@gmail.com>
2017-12-21 18:52:48 +0900

    13-1まで終了

---
+++ Practice/src/practice13/common/Hero.java
@@ -0,0 +1,13 @@
+package practice13.common;
+
+public class Hero extends Character{
+	super.character(25,10,7);
+
+	Hero hero =new Hero();
+	hero.setName("勇者");
+	System.out.println(hero.str());
+
+
+	}
+
+}
